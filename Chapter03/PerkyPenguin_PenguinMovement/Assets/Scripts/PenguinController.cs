3453c524cb0d68ae5f239dc748672ca8ba619bfd
Pavan Ramchandani <pavanr@packtpub.com>
2017-05-23 16:01:17 +0530

    Code files added

---
+++ Chapter03/PerkyPenguin_PenguinMovement/Assets/Scripts/PenguinController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PenguinController : MonoBehaviour {
+
+	public float jetpackForce = 75.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		bool jetpackActive = Input.GetButton ("Fire1");
+		if (jetpackActive == true) {
+			this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jetpackForce));
+		}
+	}
+}
